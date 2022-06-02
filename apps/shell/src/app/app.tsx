96e9f6a2128f456f7566ed73db2f3a5154259ff7
subu <subramanya4shenoy@gmail.com>
2022-06-02 22:19:09 +0530

    theme initial integration

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -1,26 +1,37 @@
+import { Button, ThemeProvider } from '@mui/material';
 import * as React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import { defaultTheme } from '../../../../libs/theme';
 
 const Social = React.lazy(() => import('social/Module'));
 
 export function App() {
   return (
-    <React.Suspense fallback={null}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+    <ThemeProvider theme={defaultTheme}>
+      <React.Suspense fallback={null}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-        <li>
-          <Link to="/social">Social</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<></>} />
+          <li>
+            <Link to="/social">Social</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Button>Button in shell</Button>
+              </>
+            }
+          />
 
-        <Route path="/social" element={<Social customProps={10} />} />
-      </Routes>
-    </React.Suspense>
+          <Route path="/social" element={<Social customProps={10} />} />
+        </Routes>
+      </React.Suspense>
+    </ThemeProvider>
   );
 }
 
---
+++ libs/theme.tsx
@@ -0,0 +1,44 @@
+import { createTheme } from '@mui/material/styles';
+import 'typeface-poppins';
+
+export const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#05f',
+    },
+    secondary: {
+      main: '#673BB7',
+    },
+    error: {
+      main: '#E50202',
+    },
+    warning: {
+      main: '#FE9431',
+    },
+    info: {
+      main: '#2754e6',
+    },
+    success: {
+      main: '#10d876',
+    },
+  },
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+  },
+  components: {
+    MuiButton: {
+      variants: [
+        {
+          props: {
+            variant: 'contained',
+          },
+          style: {
+            color: 'white',
+            borderRadius: '27px',
+          },
+        },
+      ],
+    },
+    MuiAvatar: {},
+  },
+});
