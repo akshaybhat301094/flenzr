3143b5d9bd64e99b60724851f970fc987c14dab1
subu <subramanya4shenoy@gmail.com>
2022-07-14 00:10:29 +0530

    Complete Landing Page

--- apps/shell/src/app/components/Landing/Landing.tsx
+++ apps/shell/src/app/components/Landing/Landing.tsx
@@ -1,41 +1,109 @@
 import { Button } from '@mui/material';
-import logo from '../../../../../../assets/Logos/SVG/logo-default.svg';
-import backpack from '../../../../../../assets/resources/images/camp-fire-light.svg';
+import logo from '../../../../../../assets/Logos/SVG/logo-white.svg';
+import wave from '../../../../../../assets/resources/images/wave.png';
 import { useTranslation } from 'react-i18next';
-import { Suspense } from 'react';
+import { useNavigate } from 'react-router-dom';
+import trans from '../../locales/en/translation';
+import Lottie from 'lottie-react';
+import influencer from '../../../../../../assets/Animations/influencer-day.json';
+import aurora from '../../../../../../assets/Animations/aurora.json';
 
 const LandingPage = () => {
   const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
+  const topBox = {
+    backgroundColor: '#02021c',
+  };
 
   return (
-    <Suspense>
-      <div className="flex w-screen h-screen overflow-hidden">
-        <div className="w-1/2 p-10">
-          <img className="w-32 mb-4" src={logo} alt="Flenzr" />
-          <img className="p-10 w-full" src={backpack} alt="Letsgo" />
-        </div>
-        <div className="w-1/2 p-10 flex h-screen flex-col">
-          <div className="h-1/3">
-            <Button className="float-right" variant="contained">
-              {t('login')}
-            </Button>
-          </div>
-          <div className="text-center text-3xl">
-            <div className="inline-block align-text-bottom font-bold">
-              Find the right Influencer for your Brand
-            </div>
-            <Button
-              className="mx-auto mt-24"
-              variant="contained"
-              color="success"
-            >
-              Join Now
-            </Button>
-          </div>
-        </div>
+    <div className="w-screen h-screen overflow-hidden">
+      {/* upper half of landing */}
+      <div className="w-screen h-3/4 overflow-hidden relative" style={topBox}>
+        <Lottie
+          className="w-full mx-auto opacity-80"
+          animationData={influencer}
+          loop={true}
+          renderer="svg"
+        />
+        <img
+          className="w-1/4 mt-32"
+          style={{
+            position: 'absolute',
+            top: '10%',
+            left: '50%',
+            transform: 'translate(-50%, 0%)',
+            zIndex: 100,
+          }}
+          src={logo}
+          alt="Flenzr"
+        />
+        <img
+          className="w-full -mt-1 absolute bottom-0 -mb-1"
+          src={wave}
+          alt="Flenzr"
+        />
       </div>
-    </Suspense>
+      <div className="w-screen h-1/4 bg-white absolute bottom-0 mb-10 overflow-hidden">
+        <div className="text-center text-3xl -mt-16">
+          <div className="font-bold mb-4 mt-20">{t(trans.landing.subtext)}</div>
+          <div className="mx-auto mt-4">
+            <Button
+              variant="contained"
+              className="pr-4"
+              color="success"
+              size="large"
+              onClick={() => navigate('/signup')}
+            >
+              {t(trans.landing.join)}
+            </Button>
+            <span className="px-4">or</span>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => navigate('/signin')}
+            >
+              {t(trans.landing.login)}
+            </Button>
+          </div>
+        </div>
+        <Lottie
+          className="h-full mx-auto absolute top-0"
+          animationData={aurora}
+          style={{
+            position: 'absolute',
+            top: '10%',
+            left: '50%',
+            transform: 'translate(-50%, 0%)',
+            zIndex: -1,
+          }}
+          loop={true}
+          renderer="svg"
+        />
+      </div>
+    </div>
   );
 };
 
 export default LandingPage;
+
+// <div className="w-1/2 p-10">
+//           <img className="w-32 mb-4" src={logo} alt="Flenzr" />
+//         </div>
+//         <Lottie
+//           className="w-1/2 mx-auto"
+//           animationData={influencer}
+//           loop={true}
+//           renderer="svg"
+//         />
+//         <div className="w-1/2 p-10 flex h-screen flex-col">
+//           <div className="h-1/3">
+//             <Button
+//               className="float-right"
+//               variant="contained"
+//               onClick={() => navigate('/signin')}
+//             >
+//               {t(trans.landing.login)}
+//             </Button>
+//           </div>
+//
+//         </div>
---
+++ apps/shell/src/app/locales/en/translation.js
@@ -0,0 +1,7 @@
+export default {
+  landing: {
+    login: 'login',
+    subtext: ' Find the right Influencer for your Brand',
+    join: 'Join now'
+  }
+};
