203e5c0ad6bb7404b52cc3e79c7eec99e1ebe866
subu <subramanya4shenoy@gmail.com>
2022-07-19 23:09:16 +0530

    lint and prettier

--- apps/shell/src/app/components/Onboarding/SignIn/SignIn.tsx
+++ apps/shell/src/app/components/Onboarding/SignIn/SignIn.tsx
@@ -1,43 +1,62 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+import Lottie from 'lottie-react';
+import loginYoga from '../../../../../../../assets/Animations/login-yoga.json';
+import Typography from '@mui/material/Typography/Typography';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export function SignIn() {
+  const [age, setAge] = React.useState('');
+  const { t } = useTranslation();
+
+  const outerBox = { backgroundColor: '#02021c' };
+  const centerCard = {
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          SignIn page
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div className="w-screen h-screen" style={outerBox}>
+      <Card className="w-1/2 absolute" style={centerCard}>
+        <div className="flex h-3/4">
+          <Card className="w-1/2">
+            <Lottie
+              className=""
+              animationData={loginYoga}
+              loop={true}
+              renderer="svg"
+            />
+          </Card>
+          <div className="w-1/2 p-10 flex justify-start place-items-stretch">
+            <Typography className="uppercase" variant="h6" component="h6">
+              <strong>Sign in as</strong>
+            </Typography>
+            <Select
+              labelId="demo-select-small"
+              id="demo-select-small"
+              value={age}
+              label="Age"
+              size="small"
+              onChange={handleChange}
+              className="self-start mx-10"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Flenzr</MenuItem>
+              <MenuItem value={20}>Brand</MenuItem>
+            </Select>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 }
 
