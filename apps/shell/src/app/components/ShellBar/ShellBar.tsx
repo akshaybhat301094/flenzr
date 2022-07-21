e2b674e9b3bb488854c7845e0c9abaf1488f648e
Subu <subramanya4shenoy@gmail.com>
2022-07-21 14:39:18 +0530

    I18n integration and Landing screen (#2)
    * logos and translation and landing grids
    
    * translation, resources, landing page
    
    * package-lock and translation work
    
    * gitignore update
    
    * Complete Landing Page
    
    * Landing complete- responsive
    
    * animation in login
    
    * finetuning desing
    
    * translation fix
    
    * lint and prettier

---
+++ apps/shell/src/app/components/ShellBar/ShellBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShellBar = (props: any) => {
+  return (
+    <>
+      <div> navigation bar</div>
+    </>
+  );
+};
+
+export default ShellBar;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ['./apps/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#05f',
+      secondary: '#673BB7',
+      error: '#E50202',
+      warning: '#FE9431',
+      info: '#2754e6',
+      success: '#10d876',
+      white: '#ffff',
+      black: '#000000'
+    },
+    screens: {
+      mobile: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px'
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {}
+  },
+  plugins: []
+};
