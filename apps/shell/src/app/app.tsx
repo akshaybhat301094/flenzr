6fe44fa67734d6d74e31094876d1a75270205258
subu <subramanya4shenoy@gmail.com>
2022-07-30 18:05:41 +0530

    feature page

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -2,17 +2,17 @@ import { ThemeProvider } from '@mui/material';
 import React, { lazy } from 'react';
 import { defaultTheme } from '../../../../libs/theme';
 import { Routes, Route } from 'react-router-dom';
+import { FeatureProductPage } from '@flenzr/feature/product-page';
 
 const SignUp = lazy(() => import('./components/Onboarding/SignUp/SignUp'));
 const SignIn = lazy(() => import('./components/Onboarding/SignIn/SignIn'));
-const LandingPage = lazy(() => import('./components/Landing/Landing'));
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <React.Suspense fallback={null}>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<FeatureProductPage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           {/* <Route path='*' element={<Page404 />} /> */}
